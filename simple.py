3d21c1816d27888727e0461ff738b0158f2147e2
gomer <swistak@tutamail.com>
2024-11-29 20:51:31 +0300

    comment

---
+++ simple.py
@@ -0,0 +1,6 @@
+print("введите число")
+num=int(input()) #вводим число с клавиатуры
+if num == 1 or num == 2 or num == 3 or num == 5 or num == 7 or num == 11 or num == 13 or num == 17 or num == 19 or num == 23  : #задаём условие (простые числа)
+    print("Y")
+else :
+    print("N")
